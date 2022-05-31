12e8891b5199b014c90bf662c7ccff8839c8f093
valiantone <zrjohn@yahoo.com>
2022-05-31 11:59:19 +0200

    feat: adds backend endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
-from model import forecaster, pd
+from model import forecaster, pd, db
 
 app = Flask(__name__)
 
-@app.route('/')
+
 def predict_static():
     # read static timesheets
     df = pd.read_csv("data/superhero_timesheets.csv")
@@ -14,10 +14,31 @@ def predict_static():
     print(pred)
 
     today = pd.Timestamp.now().floor("D")
-    roster = df[df.date < today]
-    actives = ", ".join(roster.superhero.unique().tolist())
+    roster = df[df.date >= today].reset_index()
+    actives = ", ".join(roster[roster.active_on_duty == 1].superhero.unique().tolist())
     n_result = roster.active_on_duty.sum()
     print(today)
+    print(roster)
+
+    screwed_perc =  float(pred - n_result)/pred * 100 if pred > n_result else 0
+    return f"<h3>Hello, NYC is {screwed_perc}% screwed today!</h3><br/>{actives} are on the job."
+
+
+@app.route('/')
+def predict_backend():
+    pred = forecaster.forecast(steps=1).astype(int).iloc[0]
+    print(pred)
+
+    # fetch results/timesheets from back-end
+    today = pd.Timestamp.now().date()
+    roster = pd.DataFrame([
+        doc.to_dict() for doc in db.collection(u"superhero_timesheets").where(u"date", u">=", str(today)).stream()
+    ])
+
+    actives = ", ".join(roster[roster.active_on_duty == 1].superhero.unique().tolist())
+    n_result = roster.active_on_duty.sum()
+    print(today)
+    print(roster)
 
     screwed_perc =  float(pred - n_result)/pred * 100 if pred > n_result else 0
     return f"<h3>Hello, NYC is {screwed_perc}% screwed today!</h3><br/>{actives} are on the job."
