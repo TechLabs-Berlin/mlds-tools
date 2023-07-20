e31959d99a9c616274abd66705d8fce383224ed2
valiantone <zrjohn@yahoo.com>
2023-07-20 20:32:36 +0200

    feat: adds sql to load copied csvs to table

--- workshops/ww_summer_23/setup.sql
+++ workshops/ww_summer_23/setup.sql
@@ -1,16 +1,18 @@
--- setup_data.sql
+-- setup.sql
 CREATE TABLE techies (
   techie_name VARCHAR(50),
   techie_id INT PRIMARY KEY,
   track VARCHAR(2),
-  highest_education VARCHAR(10),
+  highest_education VARCHAR(15),
   age INT,
   returning_techie BOOLEAN
 );
 
 CREATE TABLE edyoucated_activity (
-  activity_id SERIAL PRIMARY KEY,
   techie_id VARCHAR(4),
   session_date DATE,
   edyoucated_hours INT
 );
+
+COPY techies from '/tmp/techies.csv' DELIMITER ',' CSV HEADER;
+COPY edyoucated_activity from '/tmp/edyoucated.csv' DELIMITER ',' CSV HEADER;
