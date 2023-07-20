5377acc0677b60f5b1ef9c57d9e1826dedcc9650
valiantone <zrjohn@yahoo.com>
2023-07-20 19:46:10 +0200

    feat: adds table setup queries

--- workshops/ww_summer_23/setup_data.sql
+++ workshops/ww_summer_23/setup_data.sql
@@ -1,6 +1,15 @@
-// load_data.sql
+-- setup_data.sql
+CREATE TABLE techies (
+  techie_name VARCHAR(50),
+  techie_id INT PRIMARY KEY,
+  track VARCHAR(2),
+  highest_education VARCHAR(10),
+  age INT,
+  returning_techie BOOLEAN
+);
+
 CREATE TABLE edyoucated_activity (
-  activity_id INT PRIMARY KEY,
+  activity_id SERIAL PRIMARY KEY,
   techie_id VARCHAR(4),
   session_date DATE,
   edyoucated_hours INT
