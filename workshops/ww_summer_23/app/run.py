817a83b9369c7dc5fb6b2b957aca5988884f0a07
valiantone <zrjohn@yahoo.com>
2023-07-28 15:09:31 +0200

    feat: adds flask server

---
+++ workshops/ww_summer_23/app/run.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    print(os.listdir())
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5050))
+    app.run(debug=True, host='0.0.0.0', port=port)
