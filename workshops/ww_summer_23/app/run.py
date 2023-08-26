23211c1bb901ed5556159b2002c248bb1f11fa5a
valiantone <zrjohn@yahoo.com>
2023-08-26 10:38:53 +0200

    feat: reverts incorrect cleanup

--- workshops/ww_summer_23/app/run.py
+++ workshops/ww_summer_23/app/run.py
@@ -1,7 +1,10 @@
 import os
 
-from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from flask import Flask, flash, render_template, request, redirect
+import pandas as pd
 from psycopg2 import connect, Error
+from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = {"csv"}
 
